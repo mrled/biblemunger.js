49a0ae05b7857256a08d0ca43d769315e17af9e7
Micah R Ledbetter <me@micahrl.com>
2021-01-14 02:44:13 -0600

    Install other prereqs, etc

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["components/**/*.tsx", "pages/**/*.tsx", "styles/**/*.css"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
