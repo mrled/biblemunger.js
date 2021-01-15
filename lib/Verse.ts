8acba9d7c73c9694ca579e324e917db3953b5cfc
Micah R Ledbetter <me@micahrl.com>
2021-01-15 12:55:25 -0600

    Rename script to kjv.xml2sqlite.js

--- lib/Verse.ts
+++ lib/Verse.ts
@@ -10,10 +10,6 @@ export interface IVerse {
   verseText: string;
 }
 
-export function verseKey(verse: IVerse) {
-  return `${verse.bookNum}-${verse.chapterNum}-${verse.verseNum}`;
-}
-
 export class Verse implements IVerse {
   public constructor(
     public bookNum: number,
@@ -27,4 +23,36 @@ export class Verse implements IVerse {
   public key() {
     return verseKey(this);
   }
+}
+
+/* A Verse ID table
+ * Splits the components of a verse ID into three named properties
+ */
+export interface IVidTable {
+  bookNum: number;
+  chapterNum: number;
+  verseNum: number;
+}
+
+/* Given a vid of the form 1-1-1, return an IVidTable
+ */
+export function parseVid(vid: string): IVidTable {
+  const [bookNum, chapterNum, verseNum] = vid.split("-");
+  return {
+    bookNum: Number(bookNum),
+    chapterNum: Number(chapterNum),
+    verseNum: Number(verseNum),
+  };
+}
+
+/* Given a verse, return a Verse ID string of the form 1-1-1
+ */
+export function vid(verse: IVerse): string {
+  return `${verse.bookNum}-${verse.chapterNum}-${verse.verseNum}`;
+}
+
+/* Given a verse, return a React key that can be used to uniquely identify it in a list of verses
+ */
+export function verseKey(verse: IVerse): string {
+  return vid(verse);
 }
--- pages/munge/[search]/[replace]/index.tsx
+++ pages/munge/[search]/[replace]/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useRouter } from "next/router";
 import { GetServerSideProps } from "next";
 
 import { IVerse } from "lib/Verse";
