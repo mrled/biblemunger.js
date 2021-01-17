87136ee5426f3135b1cd6a433733fb6684f290cc
Micah R Ledbetter <me@micahrl.com>
2021-01-16 22:15:49 -0600

    Fix lib/Sanitize to work on server

--- lib/Sanitize.ts
+++ lib/Sanitize.ts
@@ -1,9 +1,22 @@
+/* HTML sanitization
+ */
+
 /* Strip any HTML from a string
  *
  * https://stackoverflow.com/questions/822452/strip-html-from-text-javascript
  */
 export function sanitizeHtml(str: string): string {
-  const tmpElement = document.createElement("div");
+  let doc;
+  if (typeof window === "undefined") {
+    // On the server, there is no 'window' or 'document', so we have to use a virtual DOM
+    const jsdom = require("jsdom");
+    const dom = new jsdom.JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
+    doc = dom.window.document;
+  } else {
+    // In the browser, we just use the real DOM
+    doc = document;
+  }
+  const tmpElement = doc.createElement("div");
   tmpElement.innerHTML = str;
   return tmpElement.textContent || tmpElement.innerText || "";
 }
