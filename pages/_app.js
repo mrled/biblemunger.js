1a4e36f7b9dc82ce78984eac9b90034cb0906afe
Micah R Ledbetter <me@micahrl.com>
2021-01-21 21:17:40 -0600

    Add Fathom

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,27 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as Fathom from "fathom-client";
+
 import "styles/tailwind.css";
 import "styles/fonts.css";
 import "styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    Fathom.load("KJZJABDT", {
+      includedDomains: ["biblemunger.micahrl.com"],
+    });
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    router.events.on("routeChangeComplete", onRouteChangeComplete);
+    return () => {
+      router.events.off("routeChangeComplete", onRouteChangeComplete);
+    };
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
